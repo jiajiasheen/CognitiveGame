35ea1dc958de01fdf678eb544c65b8b4212cbba6
Tong Zou <draco.zou@mail.utoronto.ca>
2016-11-18 22:10:17 -0500

    updated screen

--- core/src/com/cognitive/game/MainScreen.java
+++ core/src/com/cognitive/game/MainScreen.java
@@ -1,23 +1,21 @@
 package com.cognitive.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 
-public class CognitiveGame extends Game {
-    @Override
-    public void create() {
-        setScreen(new MainScreen());
-    }
+/*
+ * Created by Draco on 2016-11-18.
+ */
 
-    /*
+public class MainScreen extends ScreenAdapter {
+
     private Stage stage;
 
     @Override
-    public void create() {
+    public void show() {
         stage = new Stage(new ExtendViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight()));
         Gdx.input.setInputProcessor(stage);
 
@@ -28,12 +26,26 @@ public class CognitiveGame extends Game {
     }
 
     @Override
-    public void render() {
+    public void render(float delta) {
         Gdx.gl.glClearColor(1,1,1,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         stage.act(Gdx.graphics.getDeltaTime());
         stage.draw();
     }
-    */
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
