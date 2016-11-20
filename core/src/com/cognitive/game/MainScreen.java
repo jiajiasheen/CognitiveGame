0a14c3caf0801d20ce0bc589df552d7ddf69aa5a
Tong Zou <draco.zou@mail.utoronto.ca>
2016-11-20 15:09:53 -0500

    new screen

--- core/src/com/cognitive/game/MainScreen.java
+++ core/src/com/cognitive/game/MainScreen.java
@@ -3,8 +3,16 @@ package com.cognitive.game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.badlogic.gdx.utils.Scaling;
+import com.badlogic.gdx.utils.viewport.ScalingViewport;
+
+import java.io.Console;
 
 /*
  * Created by Draco on 2016-11-18.
@@ -13,25 +21,55 @@ import com.badlogic.gdx.utils.viewport.ExtendViewport;
 public class MainScreen extends ScreenAdapter {
 
     private Stage stage;
+    TiledMap tiledMap;
+    OrthographicCamera camera;
+    TiledMapRenderer tiledMapRenderer;
+    TestActor myActor;
+    CognitiveGame cg;
+
+    public MainScreen(CognitiveGame cg){
+        this.cg = cg;
+    }
 
     @Override
     public void show() {
-        stage = new Stage(new ExtendViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight()));
+        stage = new Stage(new ScalingViewport(Scaling.stretch, 2392, 1440, new OrthographicCamera()));
+        camera = (OrthographicCamera) stage.getCamera();
         Gdx.input.setInputProcessor(stage);
 
-        TestActor myActor = new TestActor();
+        myActor = new TestActor(stage, cg);
         stage.addActor(myActor);
         stage.addActor(myActor.imgButtonL);
         stage.addActor(myActor.imgButtonR);
+        stage.addActor(myActor.imgButtonD);
+        stage.addActor(myActor.imgButtonU);
+
+        //camera = new OrthographicCamera();
+        //camera.setToOrtho(false, myActor.texture.getWidth(), myActor.texture.getHeight());
+        //camera.update();
+        tiledMap = new TmxMapLoader().load("map.tmx");
+        tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
+        //tiledMapRenderer.setView(camera);
     }
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(1,1,1,1);
+        Gdx.gl.glClearColor(0,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        tiledMapRenderer.setView(camera);
+        tiledMapRenderer.render();
+
+        camera.position.set(myActor.actorX + myActor.texture.getWidth() / 2, myActor.actorY + myActor.texture.getHeight() / 2, 0);
+        camera.update();
 
         stage.act(Gdx.graphics.getDeltaTime());
         stage.draw();
+
+        if(myActor.ifHitPoint())
+            cg.setQuizScreen();
+        Gdx.app.log("Screen Position: ", Float.toString(Gdx.graphics.getWidth()) + " " + Float.toString(Gdx.graphics.getHeight()));
+        //Gdx.app.log();
     }
 
     @Override
@@ -46,6 +84,6 @@ public class MainScreen extends ScreenAdapter {
 
     @Override
     public void dispose() {
-
+        stage.dispose();
     }
 }
--- core/src/com/cognitive/game/TestActor.java
+++ core/src/com/cognitive/game/TestActor.java
@@ -7,29 +7,38 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 
-/**
+/*
  * Created by Draco on 2016-11-18.
  */
 
 public class TestActor extends Actor {
 
+    Stage stage;
+    CognitiveGame cg;
+
     Texture texture = new Texture(Gdx.files.internal("mario.png"));
-    Texture buttonL = new Texture(Gdx.files.internal("buttonL.png"));
-    Texture buttonR = new Texture(Gdx.files.internal("buttonR.png"));
+    Texture buttonL = new Texture(Gdx.files.internal("left.png"));
+    Texture buttonR = new Texture(Gdx.files.internal("right.png"));
+    Texture buttonD = new Texture(Gdx.files.internal("down.png"));
+    Texture buttonU = new Texture(Gdx.files.internal("up.png"));
+
     ImageButton imgButtonL;
     ImageButton imgButtonR;
+    ImageButton imgButtonD;
+    ImageButton imgButtonU;
     float actorX = 600, actorY = 600;
 
     enum STATE{
-        Left, Right, Idel
+        Left, Right, Up, Down, Idel
     }
 
     STATE state = STATE.Idel;
 
-    public TestActor(){
+    public TestActor(Stage stage, CognitiveGame cg){
         /*
         setBounds(actorX, actorY, texture.getWidth(), texture.getHeight());
         addListener(new InputListener(){
@@ -41,6 +50,9 @@ public class TestActor extends Actor {
         });
         */
 
+        this.stage = stage;
+        this.cg = cg;
+
         TextureRegion tr = new TextureRegion(buttonR);
         TextureRegionDrawable trd = new TextureRegionDrawable(tr);
         imgButtonR = new ImageButton(trd);
@@ -48,6 +60,14 @@ public class TestActor extends Actor {
         TextureRegion trL = new TextureRegion(buttonL);
         TextureRegionDrawable trdL = new TextureRegionDrawable(trL);
         imgButtonL = new ImageButton(trdL);
+
+        TextureRegion trD = new TextureRegion(buttonD);
+        TextureRegionDrawable trdD = new TextureRegionDrawable(trD);
+        imgButtonD = new ImageButton(trdD);
+
+        TextureRegion trU = new TextureRegion(buttonU);
+        TextureRegionDrawable trdU = new TextureRegionDrawable(trU);
+        imgButtonU = new ImageButton(trdU);
 
 
         imgButtonR.addListener(new InputListener(){
@@ -80,24 +100,86 @@ public class TestActor extends Actor {
             }
         });
 
-        imgButtonL.setPosition(0, 0);
-        imgButtonR.setPosition(300, 0);
+        imgButtonD.addListener(new InputListener(){
+
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                state = STATE.Down;
+                return true;
+            }
+
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                state = STATE.Idel;
+                super.touchUp(event, x, y, pointer, button);
+            }
+        });
+
+        imgButtonU.addListener(new InputListener(){
+
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                state = STATE.Up;
+                return true;
+            }
+
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                state = STATE.Idel;
+                super.touchUp(event, x, y, pointer, button);
+            }
+        });
+
+        imgButtonL.setPosition(-20, 100);
+        imgButtonR.setPosition(280, 100);
+        imgButtonD.setPosition(130, -40);
+        imgButtonU.setPosition(130, 240);
 
     }
 
     @Override
     public void draw(Batch batch, float parentAlpha) {
         batch.draw(texture, actorX, actorY);
-        //batch.draw(buttonL, 300, 0);
-        //batch.draw(buttonR, 0, 0);
     }
 
     @Override
     public void act(float delta) {
-        if(state == STATE.Right) {
-            actorX += 10;
-        }else if(state == STATE.Left)
-            actorX -= 10;
+        if(state == STATE.Right && actorX < Gdx.graphics.getWidth() - texture.getWidth()) {
+            actorX += 5;
+            setButton(5, 0);
+        }else if(state == STATE.Left && actorX > 20) {
+            actorX -= 5;
+            setButton(-5, 0);
+        }
+        else if(state == STATE.Up) {
+            actorY += 5;
+            setButton(0, 5);
+        }
+        else if(state == STATE.Down && actorY > 20) {
+            actorY -= 5;
+            setButton(0, -5);
+        }
+
+        /*
+        if(actorX == 650 && actorY == 600){
+            stage.dispose();
+            cg.setQuizScreen();
+        }
+        */
+
+    }
+
+    public void setButton(float x, float y){
+        imgButtonL.setPosition(imgButtonL.getX() + x, imgButtonL.getY() + y);
+        imgButtonR.setPosition(imgButtonR.getX() + x, imgButtonR.getY() + y);
+        imgButtonD.setPosition(imgButtonD.getX() + x, imgButtonD.getY() + y);
+        imgButtonU.setPosition(imgButtonU.getX() + x, imgButtonU.getY() + y);
+    }
+
+    public boolean ifHitPoint(){
+        if(actorX > 650 && actorX < 680 && actorY > 600 && actorY < 630)
+            return true;
+        return false;
     }
 
 }
